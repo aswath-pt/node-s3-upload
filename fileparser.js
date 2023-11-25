90e7cb62da0f212c42ed72485b6d68f02deec36d
ptaswath <ptaswath@gmail.com>
2023-11-25 19:07:29 +0530

    changes

--- fileparser.js
+++ fileparser.js
@@ -3,8 +3,6 @@ const { Upload } = require("@aws-sdk/lib-storage");
 const { S3Client, S3 } = require("@aws-sdk/client-s3");
 const Transform = require('stream').Transform;
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const region = process.env.S3_REGION;
 const Bucket = process.env.S3_BUCKET;
 
@@ -48,14 +46,9 @@ const parsefile = async (req) => {
                 // upload to S3
                 new Upload({
                     client: new S3Client({
-                        credentials: {
-                            accessKeyId,
-                            secretAccessKey
-                        },
                         region
                     }),
                     params: {
-                        ACL: 'public-read',
                         Bucket,
                         Key: `${Date.now().toString()}-${this.originalFilename}`,
                         Body: this._writeStream
